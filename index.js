8c13c34d03da7067a74597317a4bf2a63a19d18d
guoxiaoyang <guoxiaoyang0925@qq.com>
2017-11-06 20:51:58 +0800

    update heroku config

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 // require('./test');
 
  
-app.listen(process.env.SERVER_PORT || 5000, (err) => {
+app.listen(process.env.PORT || 5000, (err) => {
   if (err) {
     throw err;
   } else { 
     console.log(
 			`
-    Server is running on port: ${process.env.SERVER_PORT || 5000}
+    Server is running on port: ${process.env.PORT || 5000}
     ---
     Running on ${process.env.NODE_ENV}.
     `,
